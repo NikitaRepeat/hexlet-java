993d639fb8c3103c7f2c1a24442712d540d27eb6
Piskun Nikita <n.piskun2014@yandex.ru>
2023-04-28 00:23:51 +0300

    update README.md and EVEN.java, fix App.java

--- app/README.md
+++ app/README.md
@@ -1,1 +1,3 @@
 <a href="https://codeclimate.com/github/NikitaRepeat/hexlet-java/maintainability"><img src="https://api.codeclimate.com/v1/badges/2d9f8b4c61a7d0c05365/maintainability" /></a>
+
+https://asciinema.org/a/uSYCfA27NIi4xbQvGlD4I5w4C
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,6 +1,21 @@
 package hexlet.code;
+
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
-        Cli.welcomeUser();
+        System.out.println("Please enter the game number and press Enter. ");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.println("Your choice: ");
+        Scanner sc = new Scanner(System.in);
+        String choice = sc.nextLine();
+        if (choice.equals("1")) {
+            Cli.welcomeUser();
+        } else if (choice.equals("2")) {
+            Even.evenGame();
+        } else {
+            System.out.println("Goodbye!");
+        }
     }
 }
